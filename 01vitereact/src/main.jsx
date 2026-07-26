a93103ef294250e88c0c6e775e75ae4895a2ab99
Manthan Garg <manthangarg2024cse@gmail.com>
2026-07-26 20:08:19 +0000

    Add custom rendering logic and HTML structure for the app

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return (
+    <>
+    <h1>Custom App !</h1>
+    </>
+  )
+}
+
+
+// const reactElement = {
+//     type: 'a',  
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const reactElement = (
+  <a href="https://google.com" target="_blank">
+    Click me to visit google
+  </a>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* <MyApp /> */}
+    {reactElement}
+  </StrictMode>
 )
