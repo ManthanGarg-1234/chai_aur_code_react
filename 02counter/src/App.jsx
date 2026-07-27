c685e371a51df9b81694e083676979d0e9da9ca5
Manthan Garg <manthangarg2024cse@gmail.com>
2026-07-27 18:52:38 +0000

    feat: initialize counter app with React and Vite
    - Add main application structure in App.jsx
    - Implement counter functionality with increment and decrement features
    - Create styles for the application in App.css and index.css
    - Add SVG icons for social media and documentation
    - Include hero image and React logo assets
    - Set up Vite configuration for the project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+
+    // error (assignment to constant variable)
+    // counter = counter + 1
+    // setCounter(counter)
+    
+    setCounter(counter + 1)
+
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+      >Add value </button>
+      <br />
+
+      <button
+        onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
